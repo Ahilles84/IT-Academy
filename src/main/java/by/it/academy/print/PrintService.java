f59f7c66ec91ff33f134725f082154b29c216fcb
Oleg Kuleshov <7100570@gmail.com>
2022-12-22 21:53:34 +0300

    Hello, world

--- src/main/java/by/it/academy/print/PrintService.java
+++ src/main/java/by/it/academy/print/PrintService.java
@@ -1,7 +1,7 @@
 package by.it.academy.print;
 
 public class PrintService {
-    public static void print(String text){
+    public void print(String text){
         System.out.println(text);
     }
 }
--- src/main/java/by/it/academy/running/Main.java
+++ src/main/java/by/it/academy/running/Main.java
@@ -1,5 +1,11 @@
 package by.it.academy.running;
 
+import by.it.academy.print.PrintService;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        PrintService service = new PrintService();
+        String hello = "Hello, world!";
+        service.print(hello);
+    }
 }
