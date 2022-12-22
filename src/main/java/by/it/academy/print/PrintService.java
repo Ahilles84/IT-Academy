a8b99e8120cc9ab39734d1a081afd95ab3268bdc
Oleg Kuleshov <7100570@gmail.com>
2022-12-22 21:45:36 +0300

    add some packages

---
+++ src/main/java/by/it/academy/print/PrintService.java
@@ -0,0 +1,7 @@
+package by.it.academy.print;
+
+public class PrintService {
+    public static void print(String text){
+        System.out.println(text);
+    }
+}
---
+++ src/main/java/by/it/academy/running/Main.java
@@ -0,0 +1,5 @@
+package by.it.academy.running;
+
+public class Main {
+    
+}
