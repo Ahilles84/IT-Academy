4d09ed9622cac5622e9cdfb5cd36b310dbcfaec0
Oleg Kuleshov <7100570@gmail.com>
2022-12-26 20:02:10 +0300

    Lost file added

--- src/main/java/by/it/academy/running/Main.java
+++ src/main/java/by/it/academy/running/Main.java
@@ -5,9 +5,7 @@ import by.it.academy.print.PrintService;
 public class Main {
     public static void main(String[] args) {
         PrintService service = new PrintService();
-        String hello = "Hello, world!";
         String dev = "I'm a java programmer!";
-        service.print(hello);
         service.print(dev);
     }
 }
