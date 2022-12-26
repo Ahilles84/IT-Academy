958a1c964933bd496fe724a24156988bd1f29505
Oleg Kuleshov <7100570@gmail.com>
2022-12-26 19:03:34 +0300

    Added some sout

--- src/main/java/by/it/academy/App.java
+++ src/main/java/by/it/academy/App.java
@@ -13,6 +13,7 @@ public class App {
             for (Map.Entry<Object, Object> property: properties.entrySet()) {
                 System.out.println("property: " + property);
                 System.out.println("my name is Oleg");
+                System.out.println("I'm Sergey");
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
