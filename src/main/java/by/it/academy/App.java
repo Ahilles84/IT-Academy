997ce6fa85c6ebb391007c928df5a48cf6c0d831
Oleg Kuleshov <7100570@gmail.com>
2022-12-26 19:01:51 +0300

    My name is Oleg

--- src/main/java/by/it/academy/App.java
+++ src/main/java/by/it/academy/App.java
@@ -12,7 +12,7 @@ public class App {
             properties.load(stream);
             for (Map.Entry<Object, Object> property: properties.entrySet()) {
                 System.out.println("property: " + property);
-                System.out.println("I'm Dev!");
+                System.out.println("my name is Oleg");
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
